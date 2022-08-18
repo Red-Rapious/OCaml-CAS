3ca567b710368fc2a7f31cb4f6afa4011de7002b
Red-Rapious <red.rapious@gmail.com>
2022-08-18 19:24:42 +0200

    Added a derivative function

--- main.ml
+++ main.ml
@@ -4,12 +4,13 @@ type expression =
   | Var of string
   | Sum of expression * expression
   | Product of expression * expression
-  | Power of expression * expression
+  | Power of expression * int
 
   | Exp of expression
   | Ln of expression
   | Sin of expression
   | Cos of expression
+  | Arctan of expression
 ;;
 
 let simplify = function
@@ -23,18 +24,30 @@ let simplify = function
   | Product (Int a, Float b) -> Float ((Float.of_int a) *. b)
   | Product (Float a, Int b) -> Float ((Float.of_int b) *. a)
 
-  | Power (Int a, Int b) -> Int (Int.of_float(Float.of_int(a) ** Float.of_int(b)))
-  | Power (Float a, Float b) -> Float (a ** b)
-  | Power (Int a, Float b) -> Float (Float.of_int(a) ** b)
-  | Power (Float a, Int b) -> Float (a ** Float.of_int(b))
+  | Power (Int a, b) -> Int (Int.of_float(Float.of_int(a) ** Float.of_int(b)))
+  | Power (Float a, b) -> Float (a ** Float.of_int(b))
 
   | Ln (Exp a) -> a
   | Exp (Ln a) -> a
   | Product (Exp a, Exp b) -> Exp(Sum(a, b))
   | Sum (Ln a, Ln b) -> Ln(Product(a, b))
 
-  | Sum (Power (Sin a, Int 2), Power (Cos b, Int 2)) -> if a = b then Int 1 else Sum (Power (Sin a, Int 2), Power (Cos b, Int 2))
-
+  | Sum (Power (Sin a, 2), Power (Cos b, 2)) -> if a = b then Int 1 else Sum (Power (Sin a, 2), Power (Cos b, 2))
 
   | a -> a
+;;
+
+let rec derivative var = function
+  | Int a -> Int 0
+  | Float a -> Int 0
+  | Var v -> if v = var then Int 1 else Int 0
+  | Sum (a, b) -> Sum (derivative var a, derivative var b)
+  | Product (a, b) -> Sum (Product (derivative var a, b), Product (a, derivative var b))
+  | Power (e, b) -> Product (Product (Int b, (Product (Int 1, Int 2))), derivative var e)
+
+  | Exp a -> Product (Exp a, derivative var a)
+  | Ln a -> Product (Power (a, -1), derivative var a)
+  | Sin a -> Product (Cos a, derivative var a)
+  | Cos a -> Product (Product (Int (-1), Sin a), derivative var a)
+  | Arctan a -> Product (Power (Sum (Int 1, Power (a, 2)), -1), derivative var a)
 ;;
