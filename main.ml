fa4ecaf97bf86309e231d6ac9a89f5ec31e109b2
Red-Rapious <red.rapious@gmail.com>
2022-08-18 18:54:25 +0200

    Updated the simplify function

--- main.ml
+++ main.ml
@@ -1,13 +1,18 @@
 type expression = 
-  | Sum of expression * expression
-  | Product of expression * expression
   | Int of int
   | Float of float
   | Var of string
+  | Sum of expression * expression
+  | Product of expression * expression
   | Power of expression * expression
+
+  | Exp of expression
+  | Ln of expression
+  | Sin of expression
+  | Cos of expression
 ;;
 
-let simplifier = function
+let simplify = function
   | Sum (Int a, Int b) -> Int (a + b)
   | Sum (Float a, Float b) -> Float (a +. b)
   | Sum (Int a, Float b) -> Float ((Float.of_int a) +. b)
@@ -22,6 +27,14 @@ let simplifier = function
   | Power (Float a, Float b) -> Float (a ** b)
   | Power (Int a, Float b) -> Float (Float.of_int(a) ** b)
   | Power (Float a, Int b) -> Float (a ** Float.of_int(b))
+
+  | Ln (Exp a) -> a
+  | Exp (Ln a) -> a
+  | Product (Exp a, Exp b) -> Exp(Sum(a, b))
+  | Sum (Ln a, Ln b) -> Ln(Product(a, b))
+
+  | Sum (Power (Sin a, Int 2), Power (Cos b, Int 2)) -> if a = b then Int 1 else Sum (Power (Sin a, Int 2), Power (Cos b, Int 2))
+
 
   | a -> a
 ;;
